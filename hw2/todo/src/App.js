4992ebf65d72656f49f1a65024098cdac12a0e10
saniairfan1 <sania72310@gmail.com>
2022-04-25 17:39:44 -0500

    hw2

---
+++ hw2/todo/src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import Login from "./Login";
+import UserBar from "./UserBar";
+
+
+function App() {
+  return (
+    <div>
+      <UserBar />
+    </div>
+  )
+
+}
+  
+
+export default App;
