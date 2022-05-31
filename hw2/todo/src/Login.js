77049b49934408055fc7913976595918c28c3409
saniairfan1 <sania72310@gmail.com>
2022-05-30 20:55:56 -0500

    login and register

--- hw2/todo/src/Login.js
+++ hw2/todo/src/Login.js
@@ -1,26 +1,51 @@
 import React, { useState, useEffect, useContext } from "react";
 //import the context from ./app 
 import { ThemeContext } from "./App";
+import { useResource } from "react-request-hook";
 
 export default function Login({ dispatch }) {
     const [ username, setUsername ] = useState("")
-    function handleUsername (evt) { setUsername(evt.target.value) }
     //context
     const theme = useContext(ThemeContext)
 
+    const [ loginFailed, setLoginFailed ] = useState(false)
+    const [ password, setPassword ] = useState('')
+
+    function handleUsername (evt) { setUsername(evt.target.value) }
+    function handlePassword (evt) { setPassword(evt.target.value) }
+
+    const [ user, login ] = useResource((username, password) => ({
+        url: `/login/${encodeURI(username)}/${encodeURI(password)}`,
+        method: 'get'
+    }))
+
     useEffect(() => {
-        console.log(username)
-        return () => console.log("component removed from DOM")
-    })
+        if (user && user.data) {
+        if (user.data.length > 0) {
+        setLoginFailed(false)
+        dispatch({ type: 'LOGIN', username: user.data[0].username })
+        } else {
+        setLoginFailed(true)
+        }
+        }
+    }, [user])
+
+    // useEffect(() => {
+    //     console.log(username)
+    //     return () => console.log("component removed from DOM")
+    // })
 
     return (
-        <form onSubmit={(e) => {e.preventDefault(); dispatch({type: 'LOGIN',username})} }>
+        <div>
+        <form onSubmit={e => { e.preventDefault(); login(username, password) }}>
             <label style={{color:theme.primary}} htmlFor="login-username">Username:</label>
             <input type="text" value={username} onChange={handleUsername}
             name="login-username" id="login-username" />
             <label htmlFor="login-password">Password:</label>
-            <input type="password" name="login-password" id="login-password" />
+            <input type="password" value={password} onChange={handlePassword} name="login-password" id="login-password" />
             <input type="submit" value="Login" disabled={username.length ===0 || Login.length === 0} />
         </form>
+        {loginFailed && <span style={{ color: 'red' }}>Invalid username or password</span>}
+        </div>
     );
 }
